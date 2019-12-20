3df22c56a5c803f32e572bc6d577549dc72edac4
MisakiChen125 <1056832687@qq.com>
2019-12-20 14:34:18 +0800

    back yestody

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -28,8 +28,6 @@ export default {
   min-height: calc(100vh - 50px);
   width: 100%;
   height: 100%;
-  display: flex;
-  flex-direction: column;
   position: relative;
   overflow: hidden;
 }
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,20 +1,15 @@
 <template>
-  <div :class="classObj" class="app-wrapper">
-    <!-- <div class="app-head">
-      1212
-    </div> -->
-    <!-- <div class="main"> -->
-      <div v-if="device==='mobile'&&sidebar.opened" class="drawer-bg" @click="handleClickOutside" />
-        <sidebar class="sidebar-container"/>
+   <div :class="classObj" class="app-wrapper">
+    <div v-if="device==='mobile'&&sidebar.opened" class="drawer-bg" @click="handleClickOutside" />
+    <sidebar class="sidebar-container" />
     <div :class="{hasTagsView:needTagsView}" class="main-container">
-      <div :class="{'fixed-header':fixedHeader}">       
+      <div :class="{'fixed-header':fixedHeader}">
+        <navbar />
+       
       </div>
-      <app-main /> 
-     
-    <!-- </div>z-->
+      <app-main />
+    
     </div>
-   
-   
   </div>
 </template>
 
@@ -114,23 +109,3 @@ export default {
     width: 100%;
   }
 </style>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
