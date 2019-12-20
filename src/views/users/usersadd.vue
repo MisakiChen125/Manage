afde336c2016329b6646bbe4dbc8a06a6d5bd188
hammer <hammergwx@163.com>
2019-12-20 14:58:08 +0800

    usershow

--- src/views/users/usersadd.vue
+++ src/views/users/usersadd.vue
@@ -2,12 +2,34 @@
   <div class="usersadd">
     <h3>添加用户</h3>
     <div class="test">
-      <div class="test_item" v-for="(item,index) in 6" :key="index">
+      <div class="test_item" v-for="(item,index) in arr" :key="index">
         <div class="ti_title">
-          <p>添加用户</p>
+          <p :class="{active:i===0}" v-for="(v,i) in item.title" :key="i">{{v}}</p>
         </div>
-        <div class="ti_form"></div>
-        <div class="ti_button"></div>
+        <div class="ti_form">
+          <p v-if="item.form.filter(a=>{return a.type==='input'}).length">
+            <input
+              type="text"
+              v-for="(v,i) in item.form.filter(a=>{return a.type==='input'})"
+              :key="i"
+              :placeholder="v.text"
+            />
+          </p>
+          <p v-if="item.form.filter(a=>{return a.type==='select'}).length">
+            <select
+              name
+              id
+              v-for="(v,i) in item.form.filter(a=>{return a.type==='select'})"
+              :key="i"
+            >
+              <option :value="v.text">{{v.text}}</option>
+            </select>
+          </p>
+        </div>
+        <div class="ti_button">
+          <button>确定</button>
+          <button>重置</button>
+        </div>
       </div>
     </div>
   </div>
@@ -17,7 +39,88 @@ export default {
   props: {},
   components: {},
   data() {
-    return {};
+    return {
+      arr: [
+        {
+          title: ["添加用户", "更新用户"],
+          form: [
+            {
+              type: "input",
+              text: "请输入用户名"
+            },
+            {
+              type: "input",
+              text: "请输入密码"
+            },
+            {
+              type: "select",
+              text: "请选择身份id"
+            }
+          ]
+        },
+        {
+          title: ["添加身份"],
+          form: [
+            {
+              type: "input",
+              text: "请输入身份名称"
+            }
+          ]
+        },
+        {
+          title: ["添加api接口权限"],
+          form: [
+            {
+              type: "input",
+              text: "请输入api接口权限名称"
+            },
+            {
+              type: "input",
+              text: "请输入api接口权限url"
+            },
+            {
+              type: "input",
+              text: "请输入api接口权限方法"
+            }
+          ]
+        },
+        {
+          title: ["添加视图接口权限"],
+          form: [
+            {
+              type: "select",
+              text: "请选择已有视图"
+            }
+          ]
+        },
+        {
+          title: ["给身份设置api接口权限"],
+          form: [
+            {
+              type: "select",
+              text: "请选择身份id"
+            },
+            {
+              type: "select",
+              text: "请选择api接口"
+            }
+          ]
+        },
+        {
+          title: ["给身份设置视图权限"],
+          form: [
+            {
+              type: "select",
+              text: "请选择身份id"
+            },
+            {
+              type: "select",
+              text: "请选择视图权限"
+            }
+          ]
+        }
+      ]
+    };
   },
   computed: {},
   methods: {},
@@ -48,11 +151,6 @@ export default {
     display: flex;
     border: 1px solid #ccc;
     flex-wrap: wrap;
-    // flex-direction: column;
-    // background: #fff;
-    // border-radius: 10px;
-    // padding: 1rem;
-    // box-sizing: border-box;
     .test_item {
       width: calc(100% / 3);
       flex-shrink: 0;
@@ -78,9 +176,60 @@ export default {
           padding: 0.2rem 0.5rem;
           box-sizing: border-box;
           background: #fff;
-          &:first-child {
+          &.active {
             border: 1px solid#00f;
             color: #00f;
+          }
+        }
+      }
+      .ti_form {
+        width: 100%;
+        padding: 0.5rem 0;
+        box-sizing: border-box;
+        p {
+          width: 100%;
+          select {
+            width: calc(100% / 3);
+            margin: 0.5rem 0;
+            border: 1px solid #ccc;
+            outline: none;
+            padding: 0.15rem 0.5rem;
+            box-sizing: border-box;
+            font-size: 0.75rem;
+            display: block;
+          }
+          input {
+            width: 100%;
+            margin: 0.5rem 0;
+            border: 1px solid #ccc;
+            outline: none;
+            padding: 0.3rem 0.5rem;
+            box-sizing: border-box;
+            font-size: 0.75rem;
+          }
+        }
+      }
+
+      .ti_button {
+        width: 100%;
+        display: flex;
+        button {
+          padding: 0.3rem 0;
+          box-sizing: border-box;
+          width: calc(100% / 5);
+          border: 0;
+          background: #fff;
+          font-size: 0.85rem;
+          outline: none;
+          &:first-child {
+            width: calc(100% / 3.5);
+            background-color: #4e75ff;
+            border-color: #4e75ff;
+            box-sizing: border-box;
+            border: 0;
+            border-radius: 2px;
+            color: #fff;
+            margin-right: 0.5rem;
           }
         }
       }
