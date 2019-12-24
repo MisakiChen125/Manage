8839db63d61c7394dcfb89f8f0f32f6efa52138b
MisakiChen125 <1056832687@qq.com>
2019-12-24 21:33:23 +0800

    exam list ok

--- src/store/modules/exam.js
+++ src/store/modules/exam.js
@@ -1,23 +1,33 @@
-import {changePaper,acquire,studentList} from '@/api/exam'
+import {studentList,acquire} from '@/api/exam'
 
-const state={
-    list:[]
+const state = {
+  list: [],//获取学生试卷列表
+ kaoList: []//获取学生试卷详情
 }
-const mutations={
-    setyjList(state,payload){
-    state.list=payload
+const mutations = {
+  setyjList(state, payload) {
+    state.list = payload
+  },
+  setKaoList(state, payload) {
+    state.kaoList = payload
+  }
+
 }
-}
-const actions={
-async studentList({commit},payload){
-    let res =await studentList(payload)
-    commit(' setyjList',res)
+const actions = {
+  async studentList({commit}, payload) {
+    let res = await studentList(payload)
+    commit('setyjList', res)
+    console.log(res.exam)
+  },
+  async acquire({commit},payload){
+    let res=await acquire(payload)
+    commit('setKaoList',res)
     console.log(res)
-}
+  }
 }
 export default {
-    namespaced:true,
-    state,
-    mutations,
-    actions
+  namespaced: true,
+  state,
+  mutations,
+  actions
 }
